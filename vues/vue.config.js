191d49420a41b6dc61379a6455dddc8ecf9e7b41
lekubaba <lekubaba@126.com>
2020-08-23 23:52:05 +0800

    ok
    ss

---
+++ vues/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	publicPath:'./',
+	outputDir:'dist',
+	assetsDir:'./assets',
+	filenameHashing:false,
+	productionSourceMap:false,
+	css: {
+	    requireModuleExtension: false
+	},
+	
+	 
+	
+}
